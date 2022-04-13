43836839eba7e9259430d111c0ac1e8a10b330ef
Angel <a20190192@pucp.edu.pe>
2022-04-12 19:23:22 -0500

    primer commit

---
+++ src/main/java/pe/edu/pucp/gtics_lab2/GticsLab2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.gtics_lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/pucp/gtics_lab2/GticsLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.gtics_lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
