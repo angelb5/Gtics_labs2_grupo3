0a2984f181038a5eb6f62e2e2c0f75fa33e56a2f
Angel <a20190192@pucp.edu.pe>
2022-04-12 21:16:29 -0500

    fragmentos

--- src/main/java/pe/edu/pucp/gtics_lab2/controller/MarcaController.java
+++ src/main/java/pe/edu/pucp/gtics_lab2/controller/MarcaController.java
@@ -1,4 +1,26 @@
 package pe.edu.pucp.gtics_lab2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.pucp.gtics_lab2.entity.Marca;
+import pe.edu.pucp.gtics_lab2.repository.MarcaRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/marcas")
 public class MarcaController {
+
+    @Autowired
+    MarcaRepository marcaRepository;
+
+    @GetMapping({"/lista", ""})
+    public String listarMarcas(Model model) {
+        List<Marca> marcaList = marcaRepository.findAll();
+        model.addAttribute("marcaList", marcaList);
+        return "marcas/lista";
+    }
 }
--- src/main/java/pe/edu/pucp/gtics_lab2/repository/MarcaRepository.java
+++ src/main/java/pe/edu/pucp/gtics_lab2/repository/MarcaRepository.java
@@ -1,4 +1,7 @@
 package pe.edu.pucp.gtics_lab2.repository;
 
-public interface MarcaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.pucp.gtics_lab2.entity.Marca;
+
+public interface MarcaRepository extends JpaRepository<Marca, Integer> {
 }
